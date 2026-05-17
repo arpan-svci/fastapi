a0abfda56de64cc6c6a086df4d0f5ac5ef7ef70d
arpan <arpanmandal913.com>
2026-05-18 02:02:24 +0530

    Add user details model and enhance API endpoints

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from models import UserDetails
 
 
 app = FastAPI()
@@ -10,3 +12,34 @@ def health():
 @app.get("/greet")
 def greet():
     return {"message":"Hello Arpan"}
+
+@app.get("/{id}")
+def printId(id: int):
+    return {"message": id}
+
+@app.get("/name/{name}")
+def printName(name: str):
+    return {"message": f"hello {name}"}
+
+@app.get("/details")
+def printDetails(id :int, name :str):
+    return {"Id":id,"Name":name}
+
+@app.get("/details/{name}")
+def printLoop(name: str, number: int):
+    message = []
+    for i in range(number):
+        message.append(f"Hello {name}")
+    return {"message":message}
+
+@app.get("/optional")
+def OptionalPrint(name : Optional[str] = "User" , age : Optional[int] = 20):
+    return {"message":f"Hello {name}, your age is {age}"}
+
+@app.post("/userDetails")
+def userDetails(user: UserDetails):
+    return {"Details": user}
+
+@app.get("/details/{name:str}/{age:int}")
+def getDetails(name :str,age :int):
+    return {"message":{"name":name,"age":age}}
